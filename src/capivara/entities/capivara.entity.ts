39a6d6620cd856428714a41e6a49ef5e346f14db
LorenzoPrograming <lm.pilati@gmail.com>
2024-08-23 11:18:06 -0300

    capivara entity

--- src/capivara/entities/capivara.entity.ts
+++ src/capivara/entities/capivara.entity.ts
@@ -1,1 +1,31 @@
-export class Capivara {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: "capivaras"})
+export class Capivara {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column({nullable: false})
+    name: string;
+
+    @Column()
+    age: number;
+
+    @Column()
+    health_status: string;
+
+    @Column()
+    weigth: number;
+
+    @Column()
+    habitat: string;
+
+    @Column()
+    behavior: string;
+
+    @Column()
+    diet: string;
+
+    @Column()
+    note: string;
+}
