bd6624d83ab49b4c57111a730c70f693a867efaa
LorenzoPrograming <lm.pilati@gmail.com>
2024-08-26 19:56:38 -0300

    service

--- src/capivara/capivara.service.ts
+++ src/capivara/capivara.service.ts
@@ -1,34 +1,34 @@
-import { Injectable } from '@nestjs/common';
-import { CreateCapivaraDto } from './dto/create-capivara.dto';
-import { UpdateCapivaraDto } from './dto/update-capivara.dto';
-import { Repository } from 'typeorm';
-import { Capivara } from './entities/capivara.entity';
-import { InjectRepository } from '@nestjs/typeorm';
+  import { Injectable } from '@nestjs/common';
+  import { CreateCapivaraDto } from './dto/create-capivara.dto';
+  import { UpdateCapivaraDto } from './dto/update-capivara.dto';
+  import { Repository } from 'typeorm';
+  import { Capivara } from './entities/capivara.entity';
+  import { InjectRepository } from '@nestjs/typeorm';
 
-@Injectable()
-export class CapivaraService {
-  constructor(
-    @InjectRepository(Capivara)
-    private capivaraRepository: Repository<Capivara>){}
+  @Injectable()
+  export class CapivaraService {
+    constructor(
+      @InjectRepository(Capivara)
+      private capivaraRepository: Repository<Capivara>){}
 
-  create(createCapivaraDto: CreateCapivaraDto) {
-    this.capivaraRepository.create(createCapivaraDto);
-    return 'This action adds a new capivara';
+    async create(createCapivaraDto: CreateCapivaraDto) {
+      const capivara = this.capivaraRepository.create(createCapivaraDto);
+      return await this.capivaraRepository.save(capivara);
+    }
+
+    async findAll() {
+      return await this.capivaraRepository.find();
+    }
+
+    async findOne(id: number) {
+      return await this.capivaraRepository.findOneBy({id});
+    }
+
+    async update(id: number, updateCapivaraDto: UpdateCapivaraDto) {
+      return await this.capivaraRepository.update(id, updateCapivaraDto);
+    }
+
+    async remove(id: number) {
+      return await this.capivaraRepository.delete(id);
+    }
   }
-
-  findAll() {
-    return `This action returns all capivara`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} capivara`;
-  }
-
-  update(id: number, updateCapivaraDto: UpdateCapivaraDto) {
-    return `This action updates a #${id} capivara`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} capivara`;
-  }
-}
