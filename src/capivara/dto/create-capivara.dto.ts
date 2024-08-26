29843d29a3c7d17b5c5ae9645f42b49bfb484f03
LorenzoPrograming <lm.pilati@gmail.com>
2024-08-26 20:33:19 -0300

    validators

--- src/capivara/dto/create-capivara.dto.ts
+++ src/capivara/dto/create-capivara.dto.ts
@@ -1,10 +1,36 @@
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+
 export class CreateCapivaraDto {
+    
+    @IsNotEmpty()
+    @IsString()
     name: string;
+    
+    @IsOptional()
+    @IsNumber()
     age?: number;
+
+    @IsOptional()
+    @IsString()
     health_status?: string;
+
+    @IsOptional()
+    @IsNumber()
     weigth?: number;
+
+    @IsOptional()
+    @IsString()
     habitat?: string;
+
+    @IsOptional()
+    @IsString()
     behavior?: string;
+
+    @IsOptional()
+    @IsString()
     diet?: string;
+
+    @IsOptional()
+    @IsString()
     note?: string;
 }
